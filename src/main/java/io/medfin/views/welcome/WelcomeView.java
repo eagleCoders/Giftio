18fa8980782bb3909bb7f56ddd3399e1aad29dec
start.vaadin.com <no-reply@vaadin.com>
2024-04-27 15:30:48 +0000

    Generated project

---
+++ src/main/java/io/medfin/views/welcome/WelcomeView.java
@@ -0,0 +1,34 @@
+package io.medfin.views.welcome;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Welcome")
+@Route(value = "welcome-view")
+@RouteAlias(value = "")
+public class WelcomeView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public WelcomeView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
